36d7f94ca3fc245da4663696062b9318b0cb9e7a
Alexandro Tiago de Oliveira <48971595+alexandroTO@users.noreply.github.com>
2023-03-29 10:46:15 -0300

    Adicionado componente estilizado para lista de filmes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // @ts-nocheck
 import React, { useEffect, useState } from "react";
 import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
+import './App.css';
 
 export default function () {
   const [movieList, setMovielist] = useState([]);
@@ -12,6 +14,14 @@ export default function () {
     };
     loadAll();
   }, []);
-  
-  return <div>ola mundo</div>;
+
+  return (
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
+    </div>
+  );
 }
